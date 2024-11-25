e7a64dc1d567f7d670a0427b73cd09a21137a35c
Alex Liu <xixiliu0704@gmail.com>
2024-11-25 15:13:06 -0500

    added some vue router files

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import { createWebHistory, createRouter } from 'vue-router'
+import HomeView from '../views/HomeView.vue';
+import RegisterView from '../views/RegisterView.vue';
+import LoginView from '../views/LoginView.vue';
+
+const routes = [
+    { path: '/', component: HomeView },
+    { path: '/register', component: RegisterView },
+    { path: '/login', component: LoginView },
+    { path: '/movies', component: MoviesView },
+    { path: '/movies/:id', component: DetailView },
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+
+export default router;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,24 @@
 <script setup>
+import Header from "./components/Header.vue";
+import Hero from "./components/Hero.vue";
+import Feature from "./components/Feature.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
+    <Header />
+    <Hero />
+    <Feature />
+    <Footer />
 </template>
 
 <style scoped>
+* {
+    background-color: rgb(14, 14, 14);
+    color: #ffffff;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    margin: 0;
+    padding: 0;
+    height: 100%;
+}
 </style>
