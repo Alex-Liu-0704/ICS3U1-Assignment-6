670b3ef0578b027f942432e1ff6a23787673cea6
Alex Liu <xixiliu0704@gmail.com>
2024-11-30 22:04:56 -0500

    added css to details component

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -16,13 +16,18 @@ onMounted(async () => {
   <div v-if="response" class="details-container">
     <div class="movie-background" :style="{ backgroundImage: `url(${backgroundImage})` }">
       <div class="movie-info">
-        <img :src="`https://image.tmdb.org/t/p/w500${response.data.poster_path}`" alt="Movie Poster" class="movie-poster" />
-        <div class="movie-text">
+        <div class="left-info">
+          <img :src="`https://image.tmdb.org/t/p/w500${response.data.poster_path}`" alt="Movie Poster"
+            class="movie-poster" />
+        </div>
+        <div class="right-info">
           <h1 class="movie-title">{{ response.data.original_title }}</h1>
-          <p class="movie-overview">{{ response.data.overview }}</p>
           <p class="movie-release-date">Release Date: {{ response.data.release_date }}</p>
-          <!-- <p class=""> {{ response.data. }}></p> -->
+          <p class="movie-origin-country">Origin Country: {{ response.data.origin_country[0] }}</p>
+          <p class="movie-runtime"> {{ response.data.runtime }} minutes</p>
+          <p class="movie-average"> TMDB Rating: {{ response.data.vote_average }}/10</p>
           <a class="movie-site" :href="response.data.homepage" target="_blank">Official Movie Site</a>
+          <p class="movie-overview">{{ response.data.overview }}</p>
         </div>
       </div>
     </div>
@@ -59,7 +64,7 @@ onMounted(async () => {
   left: 0;
   right: 0;
   bottom: 0;
-  background-color: rgba(0, 0, 0, 0.75);
+  background-color: rgba(0, 0, 0, 0.85);
   z-index: 0;
 }
 
@@ -69,9 +74,52 @@ onMounted(async () => {
   display: flex;
   align-items: center;
   justify-content: center;
+  height: 750px;
+  padding: 0 100px;
 }
 
+.movie-poster {
+  width: 300px;
+  border-radius: 10px;
+}
 
+.right-info {
+  padding-left: 50px;
+}
+
+.movie-title{
+  padding-bottom: 20px;
+}
+
+.right-info p {
+  padding: 10px 0;
+}
+
+.movie-release-date, .movie-origin-country, .movie-runtime, .movie-average {
+  font-size: 14px;
+}
+
+.movie-site {
+  display: block;
+  width: 177px;
+  margin-top: 12px;
+  margin-bottom: 12px;
+  padding: 15px;
+  background-color: rgb(143, 0, 0);
+  border: 1px solid rgb(143, 0, 0);
+  color: white;
+  border-radius: 5px;
+  transition: transform 0.2s ease
+}
+
+.movie-overview {
+  font-size: 17px;
+}
+
+.movie-site:hover {
+  background-color: rgb(97, 0, 0);
+  transform: scale(1.02);
+}
 
 .trailers-title {
   font-size: 2rem;
